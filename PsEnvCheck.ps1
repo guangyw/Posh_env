9ceef2af0380feec9cf2ef79e96c44250a7fadad
Odin Heng Wang <odin@ohwang.com>
2016-01-05 21:04:52 +0000

    update PsEnvCheck

--- PsEnvCheck.ps1
+++ PsEnvCheck.ps1
@@ -1,5 +1,4 @@
 # PsEnvCheck.ps1
-
 <#
   Check current environment setup
 #>
@@ -19,10 +18,24 @@ Check-Executable "java" "java -version"
 Check-Executable "scala" "scala -version"
 Check-Executable "fsi"
 Check-Executable "python" "python --version"
+# node, npm
 
 # Check availability of environment variables
 # JAVA_HOME, SCALA_HOME, PYTHONPATH, PYTHONHOME
 
+# Language / devtools specific setup
+# pip, mvn, fake, sbt
+
 # Check the system PATH
 # 1. Is any path invalid?
 # 2. Is any path duplicated
+
+# Check other utilities
+# curl
+# gvim, atom, code, devenv, idea
+# git (and other related git command e.g. git-rebase)
+# ssh/putty
+# bash
+
+# If any of the above is wrong, is it possible to auto-fix
+# or at lesat provide suggestions or links for fixing
