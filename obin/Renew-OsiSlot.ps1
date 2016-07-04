b9f54cc983351c82ed70f05b0c8e5175e79c78e8
Odin Heng Wang <hew@microsoft.com>
2016-07-04 21:50:45 +0100

    Add Renew-OsiSlot
    which takes an OSI slot name and tries to renew the slot
    by making a web request to osiam endpoint

---
+++ obin/Renew-OsiSlot.ps1
@@ -0,0 +1,37 @@
+
+# Or Renew-OsiSlot
+
+[CmdletBinding()]
+param (
+  [parameter(Mandatory=$true)]
+  [string]$SlotName
+)
+
+$SlotName = $SlotName.Trim()
+
+$RenewUrl = "http://osiam/alterbatch.aspx?op=renew&batch=$SlotName"
+
+$resp = Invoke-WebRequest $RenewUrl -UseDefaultCredential
+
+if ($resp.StatusCode -eq 200)
+{
+  # Renew exceeding max duration also yields status code 200
+
+  # Get the expiration time from returned HTML
+  if ($resp.Content -match ".+(<table id=""body_CheckoutsTable""(.|\n)+</table>).*")
+  {
+    $table = $matches[1]
+    $table.Replace("&nbsp;", " ")
+    $xml = [xml]$table
+    return $xml.table.tr[2].td[5]
+  }
+  else
+  {
+    Write-Warning "Cannot find expiration time"
+    return $resp
+  }
+}
+else
+{
+  Write-Error "Failed to renew, status code $($resp.StatusCode)"
+}
--- obin/getiisps.ps1
+++ obin/getiisps.ps1
@@ -1,11 +1,16 @@
+# Known issue: this doesn't output anything after a reboot
+# In that case, either find the configs in another way
+# Or warn the user that processes indeed exists
+
 Get-WmiObject win32_process -Filter "name like 'w3wp.exe'" `
 |% {
+  # Write-Host $_.CommandLine
   if ($_.CommandLine -match '-ap "(\w+)-(\w*)-MSOSP(\d+)"')
   {
-    New-Object PSObject -Property @{App="/$($matches[2])"; Name=$matches[1]; Port=$matches[3]; PID=$_.ProcessId;}
+    [PSCustomObject] @{App="/$($matches[2])"; Name=$matches[1]; Port=$matches[3]; PID=$_.ProcessId;}
   }
   elseif ($_.CommandLine -match '-ap "Root-MSOSP(\d+)"')
   {
-    New-Object PSObject -Property @{App='/'; Name=''; Port=$matches[1]; PID=$_.ProcessId;}
+    [PSCustomObject] @{App='/'; Name=''; Port=$matches[1]; PID=$_.ProcessId;}
   }
 } | Sort Port, PID
