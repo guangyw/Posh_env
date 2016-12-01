eb3bed2c43491883a2a1f324471994646fbbbbc9
Odin Heng Wang <hew@microsoft.com>
2016-11-30 21:37:21 -0800

    Move old utility functions

--- lib/Utility.ps1
+++ lib/Utility.ps1
@@ -178,3 +178,41 @@ function Format-Size
     return "{0:f} B" -f $NumBytes
   }
 }
+
+
+function Write-Log
+{
+    Param(
+    [Parameter(Mandatory=$false, ValueFromPipeline=$true)]
+    [string]$content
+    )
+    if (!$content)
+    {
+        Write-Warning "Write-Log got an empty logging message"
+        return
+    }
+
+    if (!$content.Contains("`n"))
+    {
+        $content = "[$(Get-Date)] $content"
+    }
+
+    $content | Out-File -Append -FilePath $LogFilePath
+}
+
+
+function Format-TimeDuration
+{
+    Param(
+    [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+    [TimeSpan]$timeSpan
+    )
+    return "$($timeSpan.Hours):$("{0:D2}" -f $timeSpan.Minutes):$("{0:D2}" -f $timeSpan.Seconds)"
+}
+
+
+function Test-Elevated
+{
+    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator);
+}
